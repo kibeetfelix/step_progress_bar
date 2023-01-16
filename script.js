c291cb4db7eb2b74b6a2e5ea4cddf528d15a9a1e
kibeetfelix <felixkibeet@gmail.com>
2023-01-16 23:51:04 +0300

    js

--- script.js
+++ script.js
@@ -8,5 +8,17 @@ let currentChecked = 1;
 
 nextEl.addEventListener("click", () => {
     currentChecked++;
-    console.log(currentChecked);
+    if (currentChecked > stepsEl.length) {
+        currentChecked = stepsEl.length;
+    }
+    updatestepprogress();
 });
+
+function updatestepprogress() {
+    stepsEl.forEach((stepEl, idx) =>){
+        if (idx < currentChecked) {
+            stepEl.classList.add("checked");
+            stepEl.innerHTML = `<i class="fas fa-check"></i> <small>${idx === 0 ? "Start" : idx === stepsEl.length - 1 ? "Final" : "Step " + idx} </small>`;
+        }
+    }
+}
